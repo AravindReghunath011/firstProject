4b7e2e84b946a65e3d39502e40e45e9fb259d944
Aravind <aravindreghunath99@gmail.com>
2023-08-08 01:29:56 +0530

    login logout session

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,25 +2,30 @@ const User = require('../models/userModel');
 const bcrypt = require('bcryptjs')
 const nodemailer =require('nodemailer')
 const config = require('../config/config')
+const client = require('twilio')('AC2ce54817f6f67c1a6af9d684612e68ae', '378ea4bd79a471225b8ac858848f636c');
 
 module.exports ={
     getUserlogin:(req,res)=>{
-        res.render('users/login')
+        res.render('users/login',{err:req.session.loginPassErr,nouser:req.session.noUser})
 
     },
-    userLogin:async(req,res)=>{
-        console.log(req.body);
+    userLogin:async(req,res)=>{       
         let user = await User.findOne({email:req.body.email}).lean()
         if(user){
             bcrypt.compare(req.body.password,user.password).then((status)=>{
                 if(user.isVarified==1){
+                    req.session.isLoggedIn = user.name
                     res.redirect('/')
                 }else{
-                    console.log('moonji');
+                    req.session.loginPassErr = true
+                    res.redirect('/login')
+
                 }
 
             }).catch((err)=>{
-                console.log(err.message);
+
+                req.session.noUser = true
+                res.redirect('/login')
             })
             
            
@@ -30,16 +35,29 @@ module.exports ={
 
     },
     getHome:(req,res)=>{
-        res.render('users/index');
+        
+        res.render('users/index',{isLoggedIn:req.session.isLoggedIn});
+    },
+    logout:(req,res)=>{
+        req.session.isLoggedIn=false 
+        res.redirect('/')
     },
 
 
     getSignup:(req,res)=>{
-        res.render('users/signup')
+        res.render('users/signup',{emailExistErr:req.session.emailExistErr})
+        req.session.emailExistErr = false
     },
 
 
     userSignup:async(req,res)=>{
+        let emailExist = await User.findOne({email:req.body.email})
+        if(emailExist){
+
+            req.session.emailExistErr = true
+            res.redirect('/signup')
+
+        }else{
 
         otp = Math.floor(1000 + Math.random() * 9000).toString()
         req.session.otp = otp
@@ -60,36 +78,39 @@ module.exports ={
             if(err){
                 console.log('Err' , err)
             }else{
-
-                res.render('users/otp' )
                 req.session.signupData = req.body
-                
-                
+                res.render('users/otp' )
             }
         })
+
+    }
 
        
         
     },
     getOtp:async(req,res)=>{
         if(req.body.otp.join('')==req.session.otp){
-            req.body.password = await bcrypt.hash(req.body.password,10)
+            let spassword = await bcrypt.hash(req.session.signupData.password,10)
                 let use = new User({
-                    name:req.body.name,
-                    email:req.body.email,
-                    mobile:req.body.number,
-                    password:req.body.password,
-                    isAdmin:0
+                    name: req.session.signupData.name,
+                    email: req.session.signupData.email,
+                    mobile: req.session.signupData.number,
+                    password: spassword,
+                    isAdmin:0,
+                    isVarified:1
                     
                 })
 
                 await use.save().then((data)=>{
-                    console.log("Sucess" , req.session)
+                    console.log(use.name);
+                    req.session.isLoggedIn = use.name
+                    res.redirect('/')
                     
                 }).catch((err)=>{
                     console.log(err);
                 })
-            res.redirect('/')
+                
+            
         }else{
             res.send('wrong otp')
         }
@@ -144,18 +165,71 @@ module.exports ={
             }
         }) 
         }else{
-            res.send('error occured')
+            res.send('User doesnt exist')
         }
         res.render('users/otpPage',{otpErr:req.session.otpErr})
         req.session.otpErr = false
     },
     validOtp:(req,res)=>{
         if(req.body.otp.join('')==req.session.loginotp){
-            res.send('login WIth otp success')
+            req.session.isLoggedIn = 'hi,User'
+            res.redirect('/')
         }else{
             req.session.otpErr = true
             res.redirect('/otpPage')
         }
+    },
+    getforgetPassword:(req,res)=>{
+        res.render('users/forgetPassword')
+    },
+    validateNumber: async(req,res)=>{
+        let userExist = await User.findOne({mobile:req.body.number})
+        console.log(userExist);
+        if(userExist){
+            otp = Math.floor(1000 + Math.random() * 9000).toString()
+            req.session.forgetPassOtp = otp
+            req.session.forgetPassMob = req.body.number
+
+            function sendOTP(){
+            client.messages
+                .create({
+                body: `Thank you for choosing Future furniture. Use the following OTP to complete your Sign Up procedures. ${otp} `,
+                to: '+917034450703', // Text your number
+                from: '+15393287841', // From a valid Twilio number
+                })
+                .then((data)=>{
+                    res.redirect('/forgetpassOtp')
+                } );
+        
+            }
+            sendOTP()
+
+        }else{
+            res.send('Error occured')
+        }
+    },
+    getForgetpassOtp:(req,res)=>{
+        res.render('users/forgetPassOtp')
+    },
+    checkOtp:(req,res)=>{
+        if(req.body.otp.join('')==req.session.forgetPassOtp){
+            console.log('got right');
+            res.redirect('/getChangePass')
+
+        }else{
+            res.redirect('/forgetpassOtp')
+        }
+    },
+    getChangePass:(req,res)=>{
+        res.render('users/changePass')
+    },
+    changePass:async(req,res)=>{
+        let spassword = await bcrypt.hash(req.body.password,10)
+         await User.findOneAndUpdate({mobile:req.session.forgetPassMob},{password:spassword})
+        res.redirect('/login')
+        
+
+
     }
 
 
--- routes/users.js
+++ routes/users.js
@@ -22,4 +22,15 @@ router.get('/otpPage',userController.getOtpPage)
 router.post('/otpPage',userController.validOtp)
 
 router.get('/productList',productController.showProducts)
+router.get('/forgetPassword',userController.getforgetPassword)
+router.post('/forgetPassword',userController.validateNumber)
+router.get('/forgetpassOtp',userController.getForgetpassOtp)
+router.post('/forgetpassOtp',userController.checkOtp)
+router.get('/getChangepass',userController.getChangePass)
+router.post('/getChangepass',userController.changePass)
+
+
+
+
+router.get('/logout',userController.logout)
 module.exports = router;
